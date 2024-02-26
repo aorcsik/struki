03e5ecb66bb013f40c3bc48adabf3b1ea3a613f5
Antal Orcsik <aorcsik@gmail.com>
2024-02-26 08:30:04 +0100

    fix devserver properly

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 const port = config.port;
 
-app.use(express.static('.'));
+app.use(express.static('src'));
+app.use('/node_modules', express.static('node_modules'));
+app.use('/js/require.js', express.static('node_modules/requirejs/require.js'));
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
